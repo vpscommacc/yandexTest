75aded9cd425fea41de091ecdd16601243b95eab
vpscommacc <vps.commacc@yandex.ru>
2021-04-22 16:56:48 +0300

    finally done

--- yandexTest/main.py
+++ yandexTest/main.py
@@ -1,19 +1,12 @@
 import math
-
 # n = list(input())
 # s = 0
 # ai = list(map(int, input().split()))
 # s = len(ai) - 1
-# if int(ai[0]) > 1:
-#     s = -1
+# if int(ai[0]) > 1: # s = -1
 # print(s)
-
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
-
 for i in range(1, n+1):
     y = [round(math.dist((n, k), (i, i+1)))]
     print(''.join(map(str, y)), end=" ")
-
-
-
